c76b068604c79712a8fad0e1c452b7c215951e40
Johan Tique <johan.tique@analisistem.com>
2017-10-13 09:49:21 -0500

    add welcome page with login abilities / configure simple form with bootstrap 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  # Devise anywhere session stuff
+  def resource_name
+    :user
+  end
+
+  #
+  # Just a trick for having access to the devise resource anywhere
+  #
+  # @return [ User ]
+  #
+  def resource
+    @resource ||= User.new
+  end
+
+  #
+  # Just a trick for having access to the devise mapping anywhere
+  #
+  # @return [ Object ]
+  #
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
