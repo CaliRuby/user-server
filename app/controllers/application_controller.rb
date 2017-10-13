bee2088aade29ac111da80dd4988663ad1485f8b
Johan Tique <johan.tique@analisistem.com>
2017-10-13 11:06:49 -0500

    add workspace concept - a simple page in which user will be redirected after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :redirect_to_workspace
+
+  #
+  # Overrides the default behaviour by redirecting after sign in
+  #
+  # @param [ Devise::Resource ] resource
+  #
+  def after_sign_in_path_for(resource)
+    request.referrer && request.referrer != root_url ? request.referrer : '/workspace'
+  end
+
+  #
+  # Overrides the default behaviour by redirecting after sign out
+  #
+  # @param [ Devise::Resource ] resource
+  #
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+  protected
+
+  def redirect_to_workspace
+    redirect_to workspace_path if user_signed_in? && request.url == root_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
+  get :workspace, to: 'workspace#index'
   root 'welcome#index'
 end
