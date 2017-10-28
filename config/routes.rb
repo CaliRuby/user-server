7a5fb7e16502dc5f5da8eaa05582733cfb83c710
Johan Tique <johan.tique@analisistem.com>
2017-10-28 15:19:14 -0500

    add documents controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+
   get :workspace, to: 'workspace#index'
+
   namespace :users do
     resource :profiles, only: [:edit, :update] do
       get '/', to: 'profiles#index'
     end
   end
+
+  resources :documents, only: [:new, :create]
+
+  authenticated do
+    root 'workspace#index'
+  end
+
   root 'welcome#index'
 end
